171166f1fc76d146683695c06affbdb1ddb26220
Theodore Nordsieck <theodore.nordsieck@gmail.com>
2015-12-12 13:42:40 -0800

    Add help message

--- main.go
+++ main.go
@@ -3,16 +3,27 @@ package main
 import (
 	"flag"
 	"fmt"
+	"time"
 )
 
 var (
 	rawPlaylist = flag.String("playlist", "", "The youtube playlist id")
-	rawDuration = flag.String("duration", "30s", "The duration of the clip")
+	rawDuration = flag.String("duration", (30 * time.Second).String(), "The duration of the clip")
+
+	helpMsg = `
+-playlist: the id of the youtube playlist.
+-duration: the duration of the clip to select.`
 )
 
 func main() {
 	flag.Parse()
 
+	duration, err := time.ParseDuration(*rawDuration)
+	if err != nil || *rawPlaylist == "" {
+		fmt.Println(helpMsg)
+		return
+	}
+
 	fmt.Printf("playlist: '%v'\n", *rawPlaylist)
-	fmt.Printf("duration: '%v'\n", *rawDuration)
+	fmt.Printf("duration: '%v'\n", duration)
 }
